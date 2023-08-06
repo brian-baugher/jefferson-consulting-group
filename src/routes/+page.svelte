f8d9df997a6a574c201eeb6cb5cf53a9b73289bd
Brian Baugher <123590980+brian-baugher@users.noreply.github.com>
2023-08-06 14:27:35 -0400

    make landing text style global

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,23 +45,5 @@
 		background-color: transparent;
 		color: white;
 		z-index: 1;
-	}
-	h1 {
-		font-size: 10cqmin;
-		font-weight: 300;
-	}
-	p,
-	h1 {
-		margin: 0;
-		padding: 0;
-		color: white;
-		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',
-			Geneva, Verdana, sans-serif;
-		z-index: 1;
-		text-align: center;
-	}
-	p {
-		font-weight: 100;
-		text-align: center;
 	}	
 </style>
